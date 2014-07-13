d60b897ab62a37562fe860f80a5e25aa53b80a06
RichardLannigan <r.w.lannigan@hotmail.co.uk>
2014-07-13 19:59:31 +0100

    C# Windows Forms - To Do List
    A to do list coded in C# for Windows Forms.

---
+++ Programming/C# Windows Forms - To Do List/ToDoList/EditTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class EditTask : Form
+    {
+        public EditTask()
+        {
+            InitializeComponent();
+            textBox_TaskTitle.MaxLength = 16;
+            textBox_TaskDescription.MaxLength = 256;
+        }
+
+        private void _button_AddTask_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_ClearTitle_Click(object sender, EventArgs e)
+        {
+            textBox_TaskTitle.Text = "";
+        }
+
+        private void button_ClearDescription_Click(object sender, EventArgs e)
+        {
+            textBox_TaskDescription.Text = "";
+        }
+
+        private void button_ClearForm_Click(object sender, EventArgs e)
+        {
+            textBox_TaskTitle.Text = "";
+            textBox_TaskDescription.Text = "";
+        }
+    }
+}
